261077fad6c4fbec0916fd17b9fd8abd76100909
R Sánchez <rene.as.himself@gmail.com>
2019-01-20 19:36:05 -0600

    Implemented 'muter' npm package

--- server.js
+++ server.js
@@ -1,23 +1,42 @@
-'use strict';
+'use strict'
 
-var express = require('express');
-var cors = require('cors');
+const express = require('express')
+const cors = require('cors')
 
 // require and use "multer"...
+const multer = require('multer')
+const upload = multer({ dest: 'uploads/' })
 
-var app = express();
+const app = express()
 
-app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use(cors())
+app.use('/public', express.static(process.cwd() + '/public'))
 
-app.get('/', function (req, res) {
-     res.sendFile(process.cwd() + '/views/index.html');
-  });
+/*
+1. I can submit a form that includes a file upload.
+2. The form file input field  has the "name" attribute set to "upfile". We rely on this in testing.
+3. When I submit something, I will receive the file name and size in bytes within the JSON response
+*/
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res, next) => {
+      const { originalname, mimetype, size } = req.file
 
-app.get('/hello', function(req, res){
-  res.json({greetings: "Hello, API"});
-});
+      res.json({
+            name: `${originalname}`,
+            type: `${mimetype}`,
+            size: `${size}`,
+      })
+})
 
-app.listen(process.env.PORT || 3000, function () {
-  console.log('Node.js listening ...');
-});
+app.get('/', (req, res) => {
+      res.sendFile(process.cwd() + '/views/index.html')
+})
+
+app.get('/hello', (req, res) => {
+      res.json({ greetings: 'Hello, API' })
+})
+
+// listen for requests :)
+const PORT = process.env.PORT || 3000
+const listener = app.listen(PORT, () => {
+      console.log('Your app is listening on port ' + listener.address().port)
+})

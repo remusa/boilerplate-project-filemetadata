7347e17f2ff02e77da1b172e00da9ede2be9ea41
R Sánchez <rene.as.himself@gmail.com>
2019-01-20 19:36:13 -0600

    Revert "Implemented 'muter' npm package"
    This reverts commit 261077fad6c4fbec0916fd17b9fd8abd76100909.

--- server.js
+++ server.js
@@ -1,42 +1,23 @@
-'use strict'
+'use strict';
 
-const express = require('express')
-const cors = require('cors')
+var express = require('express');
+var cors = require('cors');
 
 // require and use "multer"...
-const multer = require('multer')
-const upload = multer({ dest: 'uploads/' })
 
-const app = express()
+var app = express();
 
-app.use(cors())
-app.use('/public', express.static(process.cwd() + '/public'))
+app.use(cors());
+app.use('/public', express.static(process.cwd() + '/public'));
 
-/*
-1. I can submit a form that includes a file upload.
-2. The form file input field  has the "name" attribute set to "upfile". We rely on this in testing.
-3. When I submit something, I will receive the file name and size in bytes within the JSON response
-*/
-app.post('/api/fileanalyse', upload.single('upfile'), (req, res, next) => {
-      const { originalname, mimetype, size } = req.file
+app.get('/', function (req, res) {
+     res.sendFile(process.cwd() + '/views/index.html');
+  });
 
-      res.json({
-            name: `${originalname}`,
-            type: `${mimetype}`,
-            size: `${size}`,
-      })
-})
+app.get('/hello', function(req, res){
+  res.json({greetings: "Hello, API"});
+});
 
-app.get('/', (req, res) => {
-      res.sendFile(process.cwd() + '/views/index.html')
-})
-
-app.get('/hello', (req, res) => {
-      res.json({ greetings: 'Hello, API' })
-})
-
-// listen for requests :)
-const PORT = process.env.PORT || 3000
-const listener = app.listen(PORT, () => {
-      console.log('Your app is listening on port ' + listener.address().port)
-})
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Node.js listening ...');
+});
